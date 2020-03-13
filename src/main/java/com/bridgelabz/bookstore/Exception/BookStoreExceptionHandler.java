a858b68a7643fe663f3a05fe7fe1351e4635d6d3
kajal764 <kajaldw666@gmail.com>
2020-03-13 10:51:37 +0530

    REFACTORED: Removed commented code

--- src/main/java/com/bridgelabz/bookstore/Exception/BookStoreExceptionHandler.java
+++ src/main/java/com/bridgelabz/bookstore/Exception/BookStoreExceptionHandler.java
@@ -5,17 +5,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.sql.SQLIntegrityConstraintViolationException;
-
 @ControllerAdvice
 public class BookStoreExceptionHandler {
     @ExceptionHandler(value = BookStoreException.class)
     public ResponseEntity<Object> exceptionHandler(BookStoreException book) {
         return new ResponseEntity<>(book.getMessage(), HttpStatus.BAD_REQUEST);
     }
-
-//    @ExceptionHandler(value = SQLIntegrityConstraintViolationException.class)
-//    public ResponseEntity<Object> SQLIntegretityException(SQLIntegrityConstraintViolationException e) {
-//        return new ResponseEntity<>("BOOK ALREADY EXIST", HttpStatus.BAD_REQUEST);
-//    }
 }
